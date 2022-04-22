7d3c5be50326b91e01e44daac24ac11b3cb02ffa
aedi <aedi@milkyway.un>
2022-04-22 10:28:25 +0200

    V0.2.0 pre-publish custom code execution feature; Windows binary

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# V0.2.0 (2022-04-22)
+- Added pre-publish custom code execution feature.
+- updated build.py script to work on Windows
--- build.py
+++ build.py
@@ -1,11 +1,20 @@
 import shutil
 import os
+import platform
+# converting *.ui files to *.py files
 for dirname, dirnames, filenames in os.walk("."):
     for filename in filenames:
         path = os.path.join(dirname, filename)
         if(filename[-2:] == "ui"):
             print(filename)
             os.system(f"pyuic5 {path} -o {path[:-2]}py")
-shutil.rmtree("dist")
-os.system("pyinstaller --name='IPNS-Manager' --windowed --onefile --add-data='Icon.svg:.' --hidden-import=multiaddr.codecs.ip4 --hidden-import=multiaddr.codecs.idna --hidden-import=multiaddr.codecs.uint16be LoadUI.py")
-shutil.move("dist/IPNS-Manager", "dist/IPNS-Manager_linux_x86_64")
+# shutil.rmtree("dist")
+
+if (platform.system().lower() == "windows"):
+    os.system("pyinstaller --name=IPNS-Manager --windowed --onefile --add-data=Icon.svg;. --hidden-import=multiaddr.codecs.ip4 --hidden-import=multiaddr.codecs.idna --hidden-import=multiaddr.codecs.uint16be LoadUI.py")
+    shutil.move(os.path.join("dist", "IPNS-Manager.exe"),
+                os.path.join("dist", f"IPNS-Manager_{platform.system().lower()}_{platform.machine().lower()}.exe"))
+else:
+    os.system("pyinstaller --name='IPNS-Manager' --windowed --onefile --add-data='Icon.svg:.' --hidden-import=multiaddr.codecs.ip4 --hidden-import=multiaddr.codecs.idna --hidden-import=multiaddr.codecs.uint16be LoadUI.py")
+    shutil.move(os.path.join("dist", "IPNS-Manager"),
+                os.path.join("dist", f"IPNS-Manager_{platform.system().lower()}_{platform.machine().lower()}"))
